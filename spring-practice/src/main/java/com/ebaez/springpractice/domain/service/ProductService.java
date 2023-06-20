30c3e54c7d08787f2859c064229772aae39ce2db
Esteban Baez <esteban.arturo.baez@gmail.com>
2023-06-20 17:45:55 -0500

    EBAEZ-feat: Inyeccion dependencias spring

---
+++ spring-practice/src/main/java/com/ebaez/springpractice/domain/service/ProductService.java
@@ -0,0 +1,41 @@
+package com.ebaez.springpractice.domain.service;
+
+import com.ebaez.springpractice.domain.Product;
+import com.ebaez.springpractice.domain.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+//Esta anotación también se la puede reemplazar por Component
+//La clase Service es un intermediario entre el controlador y la capa del repositorio
+//El servicio trabaja unicamente en terminos del dominio
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAll() {
+        return productRepository.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId) {
+        return productRepository.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return productRepository.getByCategory(categoryId);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public boolean delete(int productId) {
+        return getProduct(productId).map(product -> {
+            productRepository.delete(productId);
+            return true;
+        }).orElse(false);
+    }
+}
